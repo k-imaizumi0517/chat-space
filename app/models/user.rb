59d274c0c13c658e4396aa5da1dde56d471c0a75
User <user@yuzanoMacBook-ea.local>
2018-09-10 15:43:11 +0900

    edit_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :messages
+  # テキストの回答
+  # has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
 end
