6ad6b5de3c63f5f61d988c38928164b7e318390c
User <user@yuzanoMacBook-ea.local>
2018-09-10 19:09:48 +0900

    edit_side-bar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  def index
+  end
 
   def new
     @group = Group.new
@@ -24,5 +26,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
-
 end
