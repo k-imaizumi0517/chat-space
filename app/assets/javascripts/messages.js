6d3789459b88712c8e3752103c1ab9dcda8f4502
User <user@yuzanoMacBook-ea.local>
2018-09-17 19:48:27 +0900

    edit_messages.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,26 @@
 $(function() {
+  function buildHTML(message) {
+    if (message.image.url == null) {
+      message.image.url = ""
+    }
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    <img src="${message.image.url}" class="lower-message__imgage">
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +33,15 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function() {
+      alert('error');
+    });
   });
 });
+
