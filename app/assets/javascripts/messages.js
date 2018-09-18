e8edf602fc50dc9c715a4c37385eb3732720555f
User <user@yuzanoMacBook-ea.local>
2018-09-18 12:54:12 +0900

    edit_data-disable-with

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -37,6 +37,7 @@ $(function() {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function() {
