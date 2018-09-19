890594042a0d093138b721a5252ee61dc095e448
User <user@yuzanoMacBook-ea.local>
2018-09-19 20:06:58 +0900

    add_function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,6 @@
 $(function() {
   var search_list = $("#user-search-result");
+  var user_list = $("#chat-group-users");
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
@@ -12,6 +13,14 @@ $(function() {
                   ${user}
                 </div>`
     search_list.append(html);
+  }
+  function add_user(user_id, user_name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    user_list.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -36,5 +45,16 @@ $(function() {
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     });
+  });
+
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    add_user(user_id, user_name);
+    $(this).parent().remove();
+  });
+
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this).parent().remove();
   });
 });
