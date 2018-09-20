01ab05be4b45ba1c4a217ed0b97fa4cd32257c27
User <user@yuzanoMacBook-ea.local>
2018-09-20 13:11:47 +0900

    add_turbolinks

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,47 +1,49 @@
 $(function() {
-  function buildHTML(message) {
-    if (message.image == null) {
-      message.image = ""
+  $(document).on('turbolinks:load', function() {
+    function buildHTML(message) {
+      if (message.image == null) {
+        message.image = ""
+      }
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <img src="${message.image}" class="lower-message__image">
+                    </div>
+                  </div>`
+      return html;
     }
-    var html = `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                      ${message.user_name}
-                    </div>
-                    <div class="upper-message__date">
-                      ${message.created_at}
-                    </div>
-                  </div>
-                  <div class="lower-message">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                    <img src="${message.image}" class="lower-message__image">
-                  </div>
-                </div>`
-    return html;
-  }
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data) {
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.form__message').val('');
-      $('.form__submit').prop('disabled', false);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-    })
-    .fail(function() {
-      alert('error');
+    $('#new_message').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data) {
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__message').val('');
+        $('.form__submit').prop('disabled', false);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });
     });
   });
 });
